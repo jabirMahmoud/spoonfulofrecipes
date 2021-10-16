970d9564a278b24968ff09a7a28fd6d061e9f45f
jabirMahmoud <jabiralmahmoud@gmail.com>
2021-10-16 18:20:21 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7d998e5c-cd17-4a98-baa8-959cf3b53f89';
+export const MODAL_CLOSE_SEC = 2.5;
